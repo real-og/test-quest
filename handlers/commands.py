44362ecd0149f052e66cdffc2e063789df23f5c5
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-04-07 18:20:38 +0300

    step stone for push

--- handlers/commands.py
+++ handlers/commands.py
@@ -3,9 +3,30 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 import texts
-from loader import dp
+from loader import dp, bot
+import keyboards as kb
 
+needed = [7, 4, 3, 9, 1, 2]
+selected = []
 
 @dp.message_handler(commands=['start'], state="*")
 async def send_welcome(message: types.Message):
-    await message.answer(texts.greeting)
+    await message.answer(texts.greeting, reply_markup=kb.get_code_keyboard(selected))
+
+@dp.callback_query_handler()
+async def handle_num(callback: types.CallbackQuery):
+    print(callback.data)
+    selected.append(int(callback.data))
+    print('ggggggggggggggggggg')
+    if needed == selected:
+        await callback.message.answer('Успех')
+        selected = []
+
+    # if int(callback.data) is not in needed:
+    try:
+        await callback.message.edit_reply_markup(reply_markup=kb.get_code_keyboard(selected))
+    except:
+        pass
+    await bot.answer_callback_query(callback.id)
+    
+
---
+++ keyboards.py
@@ -0,0 +1,21 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+from typing import List
+
+def get_code_keyboard(selected: List[int]):
+    kb = InlineKeyboardMarkup()
+    button_1 = InlineKeyboardButton(text='☑️1️⃣' if 1 in selected else '1️⃣', callback_data='1')
+    button_2 = InlineKeyboardButton(text='☑️2️⃣' if 2 in selected else '2️⃣', callback_data='2')
+    button_3 = InlineKeyboardButton(text='☑️3️⃣' if 3 in selected else '3️⃣', callback_data='3')
+    button_4 = InlineKeyboardButton(text='☑️4️⃣' if 4 in selected else '4️⃣', callback_data='4')
+    button_5 = InlineKeyboardButton(text='☑️5️⃣' if 5 in selected else '5️⃣', callback_data='5')
+    button_6 = InlineKeyboardButton(text='☑️6️⃣' if 6 in selected else '6️⃣', callback_data='6')
+    button_7 = InlineKeyboardButton(text='☑️7️⃣' if 7 in selected else '7️⃣', callback_data='7')
+    button_8 = InlineKeyboardButton(text='☑️8️⃣' if 8 in selected else '8️⃣', callback_data='8')
+    button_9 = InlineKeyboardButton(text='☑️9️⃣' if 9 in selected else '9️⃣', callback_data='9')
+    kb.row(button_1, button_2, button_3)
+    kb.row(button_4, button_5, button_6)
+    kb.row(button_7, button_8, button_9)
+    return kb
